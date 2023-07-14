bb916a6b7c8754d3d0a55cca981fa7cc19d70254
JonFriend <jonfriend@JonFriends-MacBook-Pro.local>
2023-07-14 12:11:49 -0500

    baseline springboot app

---
+++ src/main/java/com/yep/aab_examplespringboot/AabExampleSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.yep.aab_examplespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AabExampleSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AabExampleSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yep/aab_examplespringboot/AabExampleSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yep.aab_examplespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AabExampleSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
